73dd3b8ebfa6087b3f681ffd4e1f05071f67fc50
Vsevolod <Vsevolod-Shustov@users.noreply.github.com>
2021-12-25 17:21:16 +0300

    sass import

--- src/page-index/main.js
+++ src/page-index/main.js
@@ -1,6 +1,6 @@
 require('normalize.css/normalize.css')
-require('../css/main.css')
-require('./page.css')
+require('../css/main.scss')
+require('./page.scss')
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded', 'page-index')
