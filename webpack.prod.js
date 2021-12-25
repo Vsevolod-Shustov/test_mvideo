4f427c285fb9a2753929b3223cc4c248f8964dc7
Vsevolod <Vsevolod-Shustov@users.noreply.github.com>
2021-12-25 15:18:08 +0300

    initial build

--- webpack.prod.js
+++ webpack.prod.js
@@ -17,9 +17,7 @@ module.exports = {
 
   // https://webpack.js.org/concepts/entry-points/#multi-page-application
   entry: {
-    index: './src/page-index/main.js',
-    about: './src/page-about/main.js',
-    contacts: './src/page-contacts/main.js'
+    index: './src/page-index/main.js'
   },
 
   // how to write the compiled files to disk
@@ -77,18 +75,6 @@ module.exports = {
       inject: true,
       chunks: ['index'],
       filename: 'index.html'
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/page-about/tmpl.html',
-      inject: true,
-      chunks: ['about'],
-      filename: 'about.html'
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/page-contacts/tmpl.html',
-      inject: true,
-      chunks: ['contacts'],
-      filename: 'contacts.html'
     }),
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css',
